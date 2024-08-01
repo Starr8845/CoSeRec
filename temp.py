7bf73965ab3d2da7314ce3a01eb0989df79bef69
zhangzexi <zhang-zexi@sjtu.edu.cn>
2024-08-01 11:34:26 +0800

    m

--- temp.py
+++ temp.py
@@ -2,9 +2,9 @@ from tensorboard.backend.event_processing import event_accumulator
 from torch.utils.tensorboard import SummaryWriter  
 
 import os
-input_path_folder = '/home/zzx/seqRec/CLTrys/CoSeRec/logs/Sports_and_Outdoors/CoSeRec_single_single_cl_mask_mid_low_1024'
-output_path = '/home/zzx/seqRec/CLTrys/CoSeRec/logs/Sports_and_Outdoors/CoSeRec_single_single_cl_mask_mid_low_all_1024'  # 输出只需要指定文件夹即可
- 
+input_path_folder = '/home/zzx/seqRec/CLTrys/CoSeRec/logs/Sports_and_Outdoors/CoSeRec_single_single_1024_adaptive_mask'
+output_path = '/home/zzx/seqRec/CLTrys/CoSeRec/logs/Sports_and_Outdoors/CoSeRec_single_single_1024_adaptive_mask_1'  # 输出只需要指定文件夹即可
+
 writer = SummaryWriter(output_path)  # 创建一个SummaryWriter对象
 input_path = input_path_folder
 # 读取需要修改的event文件
@@ -16,16 +16,20 @@ print(tags)
 
 for tag in tags:
     scalar_list = ea.scalars.Items(tag)
-    print(tag, scalar_list)
+    # print(tag, scalar_list)
+    print(tag, [each.step for each in scalar_list])
     
-    flag=False
+    flag=0
     for scalar in scalar_list:
-        if scalar.step==0 and flag==True:
-            break
-        if scalar.step==0 and flag==False:
-            flag=True
-        print(scalar.step)
-        writer.add_scalar(tag, scalar.value, scalar.step, scalar.wall_time)  # 添加修改后的值到新的event文件中
+        if tag in ["mask_prob/high","mask_prob/mid","mask_prob/low"]: 
+            if scalar.step==3:
+                flag+=1
+        else:
+            if scalar.step==0:
+                flag+=1
+        if flag==2:
+            writer.add_scalar(tag, scalar.value, scalar.step, scalar.wall_time)  # 添加修改后的值到新的event文件中
+            print(tag, scalar.value)
 
 writer.close()  # 关闭SummaryWriter对象
 
